b82ecdfd9e495adbb807fe8d2c701ede4cb741a8
Zixu.Deng <admin@dengzixu.com>
2025-05-05 15:36:35 +0800

    优化创建用户目录方式

--- create_monitor.sh
+++ create_monitor.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-VER='20231125_fix_1'
+VER='20250505'
 
 MONITOR_USER_NAME='monitor'
 
 PUBLIC_KEY='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvbqy1IEwIkcAiYWosYIMzuQ/bbfI5Jnwddw++/0U3M monitor'
 
-
 id ${MONITOR_USER_NAME} >/dev/null 2>&1
 
 if [ ! $? -eq 0 ]; then
-  useradd -s /bin/bash ${MONITOR_USER_NAME}
+  useradd -s /bin/bash -m -k /etc/skel ${MONITOR_USER_NAME}
 else
   echo echo "Monitor user [${MONITOR_USER_NAME}] aleardy existed."
-fi
-
-if [ ! -d "/home/${MONITOR_USER_NAME}/" ]; then
-  mkdir /home/${MONITOR_USER_NAME}
-
-  touch /home/${MONITOR_USER_NAME}/.bashrc
-  touch /home/${MONITOR_USER_NAME}/.bash_profile
-  touch /home/${MONITOR_USER_NAME}/.profile
-else
-  echo "Home dir aleardy existed."
 fi
 
 if [ ! -d "/home/${MONITOR_USER_NAME}/.ssh/" ]; then
